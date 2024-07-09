51b41609de1bbcdc3c4501d53f5c68ac3935c426
nickdieter52 <nickdieter52@gmail.com>
2024-07-09 14:34:22 +0000

    customizing site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
 export default defineNuxtConfig({
-  extends: 'content-wind'
+  extends: 'content-wind',
+
+  devtools: {
+    enabled: true
+  },
+
+  modules: ['@nuxt/ui', '@nuxt/image'],
+  compatibilityDate: '2024-07-09'
 })
